6e6ff8e3440500be36bc87303295bcd27ca9dc7d
Olga Bieliaieva <belyaevaolya0@gmail.com>
2023-02-10 14:40:36 +0200

    added markup of Task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-const galleryRef = document.querySelector('gallery');
-console.log(galleryRef);
+const galleryRef = document.querySelector(".gallery");
+
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
+galleryRef.innerHTML = galleryMarkup;
+
 function createGalleryMarkup(galleryItems) {
-    
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
 }
