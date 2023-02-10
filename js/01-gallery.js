c8f6950e6ad5ce361c85d505b12f37c6d89e2531
OlgaBieliaieva <112978931+OlgaBieliaieva@users.noreply.github.com>
2023-02-10 13:04:18 +0200

    start task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,10 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('gallery');
+console.log(galleryRef);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+    
+}
